9d75264e7958cb49993f559e7e6a5c706a99715e
Dave <davemakena@gmail.com>
2010-02-24 02:49:59 -0800

    *** basic skeleton of evolution-mutation model on
    nexties
    in evolutions and mutations
    add date_completed:datetime
    evolutions find all and not complete
    evolutions find all and children empty?
    in these evolutions
    find all and not complete
    find all and children empty?
    list

--- app/controllers/mutations_controller.rb
+++ app/controllers/mutations_controller.rb
@@ -5,18 +5,64 @@ class MutationsController < ApplicationController
   end
   
   def show
+    beautify_mutation
+  end
+  
+  def new
+    newify_mutation
+  end
+
+  def create
+    @mutation = Mutation.new(params[:mutation])
+    if @mutation.save
+      flash[:notice] = "Successfully created mutation."
+      redirect_to @mutation
+    else
+      render :action => 'new'
+    end
+  end
+  
+  def edit
+    beautify_mutation
+    @mutation = @evolution.mutations.find(params[:id])
+  end
+  
+  def update
+    beautify_mutation
+    @mutation = @evolution.mutations.find(params[:id])
+    if @mutation.update_attributes(params[:mutation])
+      flash[:notice] = "Successfully updated mutation."
+      redirect_to @mutation
+    else
+      render :action => 'edit'
+    end
+  end
+  
+  def destroy
+    beautify_mutation
+    @mutation = @evolution.mutations.find(params[:id])
+    @mutation.destroy
+    flash[:notice] = "Successfully destroyed mutation."
+    redirect_to evolution_mutations_path(@evolution)
+  end
+
+protected
+
+  def beautify_mutation
     mutation = Mutation.find(params[:id])
     if mutation.evolution_id
       @evolution = Evolution.find(mutation.evolution_id)
       @mutation = @evolution.mutations.find(params[:id])
+      @title = "Mutation"
     else
-      mutation_node = mutation.ancestors.last
-      @evolution = Evolution.find(mutation_node.evolution_id)
+      mutation_rooting = mutation.ancestors.last
+      @evolution = Evolution.find(mutation_rooting.evolution_id)
       @mutation = Mutation.find(params[:id])
+      @title = "Prior Mutation"
     end
   end
-  
-  def new
+
+  def newify_mutation
     @no_links = true
     if params[:mutation_id]
       mutation = Mutation.find(params[:mutation_id])
@@ -37,44 +83,4 @@ class MutationsController < ApplicationController
     end
   end
 
-  def create
-    @mutation = Mutation.new(params[:mutation])
-    if @mutation.save
-      flash[:notice] = "Successfully created mutation."
-      redirect_to @mutation
-    else
-      render :action => 'new'
-    end
-  end
-  
-  def edit
-    get_evolution_from_mutation
-    @mutation = @evolution.mutations.find(params[:id])
-  end
-  
-  def update
-    get_evolution_from_mutation
-    @mutation = @evolution.mutations.find(params[:id])
-    if @mutation.update_attributes(params[:mutation])
-      flash[:notice] = "Successfully updated mutation."
-      redirect_to @mutation
-    else
-      render :action => 'edit'
-    end
-  end
-  
-  def destroy
-    get_evolution_from_mutation
-    @mutation = @evolution.mutations.find(params[:id])
-    @mutation.destroy
-    flash[:notice] = "Successfully destroyed mutation."
-    redirect_to mutations_url
-  end
-
-protected
-
-  def get_evolution_from_mutation
-    mutation = Mutation.find(params[:id])
-    @evolution = Evolution.find(mutation.evolution_id)
-  end
 end
