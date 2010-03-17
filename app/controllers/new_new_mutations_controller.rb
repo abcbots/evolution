3f260ebc9e79c561d6a62ea3bcbdce6de98a8804
BB <BB@localhost.localdomain>
2010-03-17 03:28:18 -0700

    *** finished refactoring mutations ***
    now add link menus to application;
    delete old menus
    smooth and copy to evolutions
    hook mutations into evolutions
    ready to build layout....

--- app/controllers/new_new_mutations_controller.rb
+++ app/controllers/new_new_mutations_controller.rb
@@ -2,21 +2,18 @@ class MutationsController < ApplicationController
 
 # ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** 
 #
-# new | clone | move | destroy
-# current | current_single
-# root | parent | current | child
-# new: root | parent | current | child
+# Mutation: Tree | New | Clone | Move | Destroy # layouts/mutations/menu
 
-# clone: current | current_single
-# move: current | current_single
+# New: Root | Parent | Current | Child # layouts/mutatons/new
+# Clone: Current | Current uni # layouts/mutations/clone
+# Move: Current | Current uni # layouts/mutations/move
+# Destroy: Current | Current uni # layouts/mutations/destroy
 
-# clone_current_to: root | parent | current | child | cancel
-# clone_current_single_to: root | parent | current | child | cancel
+# Clone to: Root | Parent | Current | Child | Cancel # layouts/mutations/clone_to
+# Clone uni to: Root | Parent | Current | Child | Cancel # layouts/mutations/clone_uni_to
+# Move to: Root | Parent | Current | Child | Cancel # layouts/mutations/move_to
+# Move uni to: Root | Parent | Current | Child | Cancel # layouts/mutations/move_uni_to
 
-# move_current_to: root | parent | current | child | cancel
-# move_current_single_to: root | parent | current | child | cancel
-
-# destroy: current | current_single
 #
 # ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** 
 
@@ -27,86 +24,37 @@ class MutationsController < ApplicationController
 # ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** 
 
   def index
-    go_index
+    @evolution = Evolution.find(params[:evolution_id]
+    @mutations = @evolution.mutations.all
   end
   def show
-    go_show
-  end
-  def new
-    go_new
-  end
-  def create
-    go_create
+    get_mutations
   end
   def update
-    go_update
-  end
-  def destroy
-    go_destroy
-  end
-
-# ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** 
-#
-#                                   new
-#
-# ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** 
-
-  def new_root
-    go_new_root
-  end
-  def new_parent
-    go_new_parent
-  end
-  def new_current
-    go_new_current
-  end
-  def new_child
-    go_new_child
+    if @mutation_current.update_attributes(params[:mutation])
+      flash_success
+      redirect_to @mutation_current
+    else
+      flash_fail
+      redirect_to @mutation_current
+    end
   end
 
 # ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** 
 #
-#                                   set
+#                                   save 
 #
 # ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** 
 
-  def set_clone_current
-    go_set_clone_current
+  def save_new
+    if @mutation_new.save
+      flash_success
+      redirect_to @mutation_new
+    else
+      flash_fail
+      redirect_to @mutation_current
+    end
   end
-  def set_clone_current_single
-    go_set_clone_current_single
-  end
-  def set_move_current
-    go_set_move_current
-  end
-  def set_move_current_single
-    go_set_move_current_single
-  end
-
-# ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** 
-#
-#                                  cancel
-#
-# ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** 
-
-  def cancel_clone_current
-    go_cancel_clone_current
-  end
-  def cancel_clone_current_single
-    go_cancel_clone_current_single
-  end
-  def cancel_move_current
-    go_cancel_move_current
-  end
-  def cancel_move_current_single
-    go_cancel_move_current_single
-  end
-
-# ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** 
-#
-#                                  clone
-#
-# ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** 
 
   def save_clone
     if @mutation_clone.save
@@ -118,74 +66,16 @@ class MutationsController < ApplicationController
       redirect_to @mutation_current
     end
   end
-  
-  def save_clone_single
-    if @mutation_clone.save
+  def save_clone_uni
+    if @mutation_clone_uni.save
       flash_success
-      session[:mutation_clone_single_id] = nil
-      redirect_to @mutation_clone_single
+      session[:mutation_clone_uni_id] = nil
+      redirect_to @mutation_clone_uni
     else
       flash_fail
       redirect_to @mutation_current
     end
   end
-
-  def clone_from(object, is_single=false)
-    @mutation_clone = Mutation.new
-    if is_single
-      copy_over_to @mutation_clone, object
-      save_clone
-    else # is many
-      clone_children_from object
-    end
-    #save_clone
-    ############################
-  end
-
-  def clone_children_from(object, object_parent)
-    for mutation in object.children
-      mutation_clone = Mutation.new
-      copy_over_to mutation_clone, mutation
-      mutation_clone.save
-      clone_children_from mutation, mutation_clone
-    end
-    save_clone
-  end
-
-  def copy_over_to(subject, object)
-  end
-  
-  def clone_to_root
-    go_clone_to_root
-  end
-  def clone_to_parent
-    go_clone_to_parent
-  end
-  def clone_to_current
-    go_clone_to_current
-  end
-  def clone_current_to_child
-    go_clone_to_child
-  end
-
-  def clone_single_to_root
-    go_clone_single_to_root
-  end
-  def clone_single_to_parent
-    go_clone_single_to_parent
-  end
-  def clone_single_to_current
-    go_clone_single_to_current
-  end
-  def clone_single_to_child
-    go_clone_single_to_child
-  end
-
-# ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** 
-#
-#                                   move
-#
-# ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** 
 
   def save_move
     if @mutation_move.save
@@ -197,81 +87,228 @@ class MutationsController < ApplicationController
       redirect_to @mutation_current
     end
   end
-  
-  def save_move_single
-    if @mutation_move.save
+  def save_move_uni
+    if @mutation_move_uni.save
       flash_success
-      session[:mutation_move_single_id] = nil
-      redirect_to @mutation_move_single
+      session[:mutation_move_uni_id] = nil
+      redirect_to @mutation_move_uni
     else
       flash_fail
       redirect_to @mutation_current
     end
   end
   
+
+# ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** 
+#
+#                                   new
+#
+# ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** 
+
+  def new_root
+    get_mutations
+    @mutation_new = Mutation.new
+    place_at_root @mutation_new
+    save_new
+  end
+  def new_parent
+    get_mutations
+    @mutation_new = Mutation.new
+    place_at_parent @mutation_new
+    save_new
+  end
+  def new_current
+    get_mutations
+    @mutation_new = Mutation.new
+    place_at_current @mutation_new
+    save_new
+  end
+  def new_child
+    get_mutations
+    @mutation_new = Mutation.new
+    place_at_child @mutation_new
+    save_new
+  end
+
+# ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** 
+#
+#                                   set
+#
+# ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** 
+
+  def set_clone
+    get_mutations
+    session[:mutation_clone_id] = @mutation_current.id # set clone
+    redirect_to @mutation_current # redirect to current
+  end
+  def set_clone_uni
+    get_mutations
+    session[:mutation_clone_uni_id] = @mutation_current.id # set clone uni
+    redirect_to @mutation_current # redirect to current
+  end
+  def set_move
+    get_mutations
+    session[:mutation_move_id] = @mutation_current.id # set move
+    redirect_to @mutation_current # redirect to current
+  end
+  def set_move_uni
+    get_mutations
+    session[:mutation_move_uni_id] = @mutation_current.id # set move uni
+    redirect_to @mutation_current # redirect to current
+  end
+
+# ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** 
+#
+#                                  cancel
+#
+# ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** 
+
+  def cancel_clone
+    get_mutations
+    session[:mutation_clone_id]=nil # clear session
+    redirect_to @mutation_current # redirect to current
+  end
+  def cancel_clone_uni
+    get_mutations
+    session[:mutation_clone_uni_id]=nil # clear session
+    redirect_to @mutation_current # redirect to current
+  end
+  def cancel_move
+    get_mutations
+    session[:mutation_move_id]=nil # clear session
+    redirect_to @mutation_current # redirect to current
+  end
+  def cancel_move_uni
+    get_mutations
+    session[:mutation_move_uni_id]=nil # clear session
+    redirect_to @mutation_current # redirect to current
+  end
+
+# ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** 
+#
+#                                  clone
+#
+# ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** 
+
+  def copy_over_to_from(subject_to, object_from)
+    #copy script
+  end
+  def clone_children_to_from(subject_to, object_from)
+    for mutation in object_from.children
+      mutation_clone = Mutation.new
+      copy_over_to_from mutation_clone, mutation
+      mutation_clone.save
+      clone_children_to_from mutation_clone, mutation
+    end
+  end
+
+  def make_clone
+    get_mutations
+    mutation_clone = Mutation.new
+    copy_over_to_from mutation_clone, @mutation_clone
+    mutation_clone.save
+    clone_children_to_from mutation_clone, @mutation_clone
+    @mutation_clone = mutation_clone
+  end
+  def make_clone_uni
+    get_mutations
+    mutation_clone_uni = Mutation.new
+    copy_over_to_from mutation_clone_uni, @mutation_clone_uni
+    @mutation_clone_uni = mutation_clone_uni
+  end
+
+  def clone_to_root
+    make_clone
+    place_at_root @mutation_clone
+    save_clone
+  end
+  def clone_to_parent
+    make_clone
+    place_at_parent @mutation_clone
+    save_clone
+  end
+  def clone_to_current
+    make_clone
+    place_at_current @mutation_clone
+    save_clone
+  end
+  def clone_to_child
+    make_clone
+    place_at_child @mutation_clone
+    save_clone
+  end
+  
+  def clone_uni_to_root
+    make_clone_uni
+    place_at_root @mutation_clone_uni
+    save_clone_uni
+  end
+  def clone_uni_to_parent
+    make_clone_uni
+    place_at_parent @mutation_clone_uni
+    save_clone_uni
+  end
+  def clone_uni_to_current
+    make_clone_uni
+    place_at_current @mutation_clone_uni
+    save_clone_uni
+  end
+  def clone_uni_to_child
+    make_clone_uni
+    place_at_child @mutation_clone_uni
+    save_clone_uni
+  end
+
+# ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** 
+#
+#                                   move
+#
+# ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** 
+
   def move_to_root
     get_mutations
-    @mutation_move.evolution_id = @mutation_root.evolution_id
-    @mutation_move.mutation_id = nil
+    place_at_root @mutation_move
     save_move
   end
   def move_to_parent
     get_mutations
-    if @mutation_super # if super
-      attach_to @mutation_move, @mutation_super, true
-    else # @mutation_parent
-      attach_to @mutation_move, @mutation_parent
-    end
-    attach_to @mutation_current, @mutation_move
+    place_at_parent @mutation_move
     save_move
   end
   def move_to_current
     get_mutations
-    if @mutation_super # if super
-      attach_to @mutation_move, @mutation_super, true
-    else # @mutation_parent
-      attach_to @mutation_move, @mutation_parent
-    end
+    place_at_current @mutation_move
     save_move
   end
   def move_to_child
-    attach_to @mutation_move, @mutation_current
+    get_mutations
+    place_at_child @mutation_move
     save_move
   end
 
-  def move_single_to_root
+  def move_uni_to_root
     get_mutations
-    singleize_mutation @mutation_move_single
-    @mutation_move_single.evolution_id = @mutation_root.evolution_id
-    @mutation_move_single.mutation_id = nil
-    save_move_single
+    distill_mutation @mutation_move_uni
+    place_at_root @mutation_move_uni
+    save_move_uni
   end
-  def move_single_to_parent
+  def move_uni_to_parent
     get_mutations
-    singleize_mutation @mutation_move_single
-    if @mutation_super # if super
-      attach_to @mutation_move_single, @mutation_super, true
-    else # @mutation_parent
-      attach_to @mutation_move_single, @mutation_parent
-    end
-    attach_to @mutation_current, @mutation_move_single
-    save_move_single
+    distill_mutation @mutation_move_uni
+    place_at_parent @mutation_move_uni
+    save_move_uni
   end
-  def move_single_to_current
+  def move_uni_to_current
     get_mutations
-    singleize_mutation @mutation_move_single
-    if @mutation_super # if super
-      attach_to @mutation_move_single, @mutation_super, true
-    else # @mutation_parent
-      attach_to @mutation_move_single, @mutation_parent
-    end
-    save_move
+    distill_mutation @mutation_move_uni
+    place_at_current @mutation_move_uni
+    save_move_uni
   end
-  def move_single_to_child
+  def move_uni_to_child
     get_mutations
-    singleize_mutation @mutation_move_single
-    attach_to @mutation_move_single, @mutation_current
-    save_move
+    distill_mutation @mutation_move_uni
+    place_at_child @mutation_move_uni
+    save_move_uni
   end
 
 # ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** 
@@ -294,9 +331,9 @@ class MutationsController < ApplicationController
       redirect_to @mutation_current
     end
   end
-  def destroy_current_single
+  def destroy_current_uni
     get_mutations
-    singleize_mutation @mutation_current
+    distill_mutation @mutation_current
     if @mutation_current.destroy
       flash_success
       if @mutation_parent
@@ -310,23 +347,50 @@ class MutationsController < ApplicationController
     end
   end
 
-#protected
-
 # ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** 
 #
 #                                 helpers                     
 #
 # ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** 
 
+  def place_at_root(subject)
+    subject.evolution_id = @mutation_root.evolution_id
+    subject.mutation_id = nil
+    subject.save
+  end
+  def place_at_parent(subject)
+    if @mutation_super # if super
+      attach_to subject, @mutation_super, true
+    else # @mutation_parent
+      attach_to subject, @mutation_parent
+    end
+    attach_to @mutation_current, subject
+    @mutation_current.save
+    subject.save
+  end
+  def place_at_current(subject)
+    if @mutation_super # if super
+      attach_to subject, @mutation_super, true
+    else # @mutation_parent
+      attach_to subject, @mutation_parent
+    end
+    subject.save
+  end
+  def place_at_child(subject)
+    attach_to subject, @mutation_current
+    subject.save
+  end
+
+
 # get_mutations
 # @mutation_super(if exists)
 # @mutation_root
 # @mutation_parent(if exists)
 # @mutation_current
-# @mutation_move_current(if exists)
-# @mutation_move_current_single(if exists)
-# @mutation_clone_current(if exists)
-# @mutation_clone_current_single(if exists)
+# @mutation_move(if exists)
+# @mutation_move_uni(if exists)
+# @mutation_clone(if exists)
+# @mutation_clone_uni(if exists)
 
   def get_mutations(pass=params[:id])
     @mutation_root = @mutation_current.ancestors.last # get root
@@ -337,19 +401,17 @@ class MutationsController < ApplicationController
     if @mutation_current.mutation_id
       @mutation_parent = Mutation.find(@mutation_current.mutation_id)
     end # get parent of current
-
     if session([:mutation_clone_id])
       @mutation_clone = Mutation.find(session[:mutation_clone_id]) 
     end # get clone_current from session
-    if session([:mutation_clone_single_id])
-      @mutation_clone_single = Mutation.find(session[:mutation_clone_single_id]) 
-    end # get clone_current_single from session
-
+    if session([:mutation_clone_uni_id])
+      @mutation_clone_uni = Mutation.find(session[:mutation_clone_uni_id]) 
+    end # get clone_current_uni from session
     if session([:mutation_move_id])
       @mutation_move = Mutation.find(session[:mutation_move_id]) 
     end # get move_current from session
-    if session([:mutation_move_single_id])
-      @mutation_move_single = Mutation.find(session[:mutation_move_single_id]) 
+    if session([:mutation_move_uni_id])
+      @mutation_move_uni = Mutation.find(session[:mutation_move_uni_id]) 
     end
   end
 
@@ -357,15 +419,15 @@ class MutationsController < ApplicationController
 # object = the mutation that will remain un-changed
 # whole = indicates if the subject includes all children
 
-  def singleize_mutation(subject)
+  def distill_mutation(subject)
     if subject.mutation_id # if subject has parent
       subject_parent = Mutation.find(subject.mutation_id)
       attach_children_to subject, subject_parent
     else # subject has super
       subject_super = Evolution.find(subject.evolution_id)
-      attach_children_to subject, subject_super
+      attach_children_to subject, subject_super, true
     end
-    subject.mutation_id = evolution_id = nil
+    subject.mutation_id = subject.evolution_id = nil
     subject.save
   end
 
