2df49d46effc75675affd533d5835a8067e6f82c
BB <BB@localhost.localdomain>
2010-03-28 19:46:12 -0700

    *** evolution > do > new > mutation ***

--- test/functional/evolutions_controller_test.rb
+++ test/functional/evolutions_controller_test.rb
@@ -10,41 +10,7 @@ class EvolutionsControllerTest < ActionController::TestCase
     get :show, :id => Evolution.first
     assert_template 'show'
   end
-  
-  def test_new
-    get :new
-    assert_template 'new'
-  end
-  
-  def test_create_invalid
-    Evolution.any_instance.stubs(:valid?).returns(false)
-    post :create
-    assert_template 'new'
-  end
-  
-  def test_create_valid
-    Evolution.any_instance.stubs(:valid?).returns(true)
-    post :create
-    assert_redirected_to evolution_url(assigns(:evolution))
-  end
-  
-  def test_edit
-    get :edit, :id => Evolution.first
-    assert_template 'edit'
-  end
-  
-  def test_update_invalid
-    Evolution.any_instance.stubs(:valid?).returns(false)
-    put :update, :id => Evolution.first
-    assert_template 'edit'
-  end
-  
-  def test_update_valid
-    Evolution.any_instance.stubs(:valid?).returns(true)
-    put :update, :id => Evolution.first
-    assert_redirected_to evolution_url(assigns(:evolution))
-  end
-  
+
   def test_destroy
     evolution = Evolution.first
     delete :destroy, :id => evolution
